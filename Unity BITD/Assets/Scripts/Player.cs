8233f3201f375edf92d59ffcffc982caf13cde17
YIvan <megavirus74@gmail.com>
2015-04-20 02:07:35 +0500

    Added player anim and changed rotation behaviour

--- Unity BITD/Assets/Scripts/Player.cs
+++ Unity BITD/Assets/Scripts/Player.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Player : MonoBehaviour {
     public GameObject bump;
     public GameObject player;
+    private Vector3 mousePosition;
     // Use this for initialization
     private void Start() {
         //начальное направление игрока - ось OX!	
@@ -12,19 +14,27 @@ public class Player : MonoBehaviour {
     private void Update() {
         //движение игрока за мышкой
         if (!LevelManager.isGameOver) {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            float angle = Vector2.Angle(Vector2.right, mousePosition - transform.position);
-            transform.eulerAngles = new Vector3(0f, 0f, transform.position.y < mousePosition.y ? angle : -angle);
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (mousePosition.x >= Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, 0, 0)).x) transform.localScale = new Vector3(-Math.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            if (mousePosition.x < Camera.main.ScreenToWorldPoint(new Vector3(Screen.width/2,0,0)).x) transform.localScale = new Vector3(Math.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            //float angle = Vector2.Angle(Vector2.right, mousePosition - transform.position);
+            //transform.eulerAngles = new Vector3(0f, 0f, transform.position.y < mousePosition.y ? angle : -angle);
 
-            //создание стрелки по нажатию на левую кнопку мыши
+            //создание шишки по нажатию на левую кнопку мыши
 
 
             if (Input.GetMouseButtonDown(0)) {
-                var cloneBump = (GameObject) Instantiate(bump);
-                Vector3 bumpDir = mousePosition - player.transform.position;
-                cloneBump.transform.rotation = Quaternion.LookRotation(Vector3.forward, bumpDir);
-                cloneBump.rigidbody2D.AddRelativeForce(new Vector2(0f, 500f));
+                gameObject.GetComponent<Animator>().Play("PlayerThrow");
+                Invoke("InstBump", 0.5f);
+
             }
         }
+    }
+
+    void InstBump() {
+        var cloneBump = (GameObject)Instantiate(bump, transform.position + transform.up, transform.rotation);
+        Vector3 bumpDir = mousePosition - player.transform.position;
+        cloneBump.transform.rotation = Quaternion.LookRotation(Vector3.forward, bumpDir);
+        cloneBump.rigidbody2D.AddRelativeForce(new Vector2(0f, 500f));
     }
 }
